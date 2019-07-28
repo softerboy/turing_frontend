5100c4432b79db124b835464de3ab62f48dabae9
Fakhriddin Umarov <f.umarov@stylemix.net>
2019-07-28 16:03:28 +0500

    add apollo client

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
 import Home from './pages/Home'
 import Auth from './pages/Auth'
 import PrivateRoute from './components/auth/PrivateRoute'
 import AppProvider from './components/context/AppContext'
 
+const client = new ApolloClient()
+
 const App = () => (
-  <AppProvider>
-    <BrowserRouter>
-      <Switch>
-        <Route
-          exact
-          name="home"
-          path={['/', '/home', '/index']}
-          component={Home}
-        />
-        {/*
+  <ApolloProvider client={client}>
+    <AppProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            name="home"
+            path={['/', '/home', '/index']}
+            component={Home}
+          />
+          {/*
         The role of key prop is clear (i.e. rerender)
         login ind register form fields in every page transition.
         Without it forms same fields value keeps their
         value between page transition
       */}
-        <Route exact name="login" path="/login">
-          <Auth key="login" />
-        </Route>
-        <Route exact name="register" path="/register">
-          <Auth key="register" register />
-        </Route>
-        <PrivateRoute exact path="/secret" component={() => <h1>Secret</h1>} />
-      </Switch>
-    </BrowserRouter>
-  </AppProvider>
+          <Route exact name="login" path="/login">
+            <Auth key="login" />
+          </Route>
+          <Route exact name="register" path="/register">
+            <Auth key="register" register />
+          </Route>
+          <PrivateRoute
+            exact
+            path="/secret"
+            component={() => <h1>Secret</h1>}
+          />
+        </Switch>
+      </BrowserRouter>
+    </AppProvider>
+  </ApolloProvider>
 )
 
 export default App

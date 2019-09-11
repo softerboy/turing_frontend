3d0f968d11f0314ed2fed5ae9e4a221b8427c9aa
Fakhriddin Umarov <f.umarov@stylemix.net>
2019-09-11 21:52:21 +0500

    handle empty cart action

--- src/components/cart/CartModal.js
+++ src/components/cart/CartModal.js
@@ -1,33 +1,45 @@
 import React, { useContext } from 'react'
-import { Button, Modal } from 'antd'
+import { Button, Modal, Popconfirm } from 'antd'
 import * as PropTypes from 'prop-types'
+import { useTranslation } from 'react-i18next'
+import { useApolloClient, useMutation } from '@apollo/react-hooks'
 
 import CartList from './CartList'
 import { propTypeCarts } from './prop-types'
 import './CartModal.less'
 import { AppContext } from '../context/AppContext'
+import EMPTY_CART_MUTATION from '../../graphql/empty-cart-mutation.graphql'
+import { getCartId } from '../../common/utils'
+import { CART_ID_KEY } from '../../common/constants'
 
 const btnProps = {
   shape: 'round',
   size: 'large',
 }
 
-const BACK_TO_SHOP = 'ACTION_BACK_TO_SHOP'
 const CHECKOUT = 'ACTION_CHECKOUT'
+const EMPTY_CART = 'ACTION_EMPTY_CART'
 
 /* eslint-disable react/jsx-props-no-spreading */
 const CartModalFooter = ({ onButtonClick }) => {
+  const { t } = useTranslation()
   return (
     <div className="cartModalFooter" style={{ padding: '15px 25px' }}>
-      <Button {...btnProps} onClick={e => onButtonClick(e, BACK_TO_SHOP)}>
-        Back to Shop
-      </Button>
+      <Popconfirm
+        overlayStyle={{ zIndex: 999999999999 }}
+        title={t('Are you sure you want to clear all items?')}
+        okText={t('Yes')}
+        cancelText={t('No')}
+        onConfirm={e => onButtonClick(e, EMPTY_CART)}
+      >
+        <Button {...btnProps}>{t('Empty cart')}</Button>
+      </Popconfirm>
       <Button
         {...btnProps}
         type="primary"
         onClick={e => onButtonClick(e, CHECKOUT)}
       >
-        Checkout
+        {t('Checkout')}
       </Button>
     </div>
   )
@@ -43,9 +55,25 @@ CartModalFooter.defaultProps = {
 
 const CartModal = ({ showModal, onCancel, carts }) => {
   const { cartTotalAmount } = useContext(AppContext)
-  const handleFooterActions = (e, action) => {
-    if (action === BACK_TO_SHOP) onCancel()
-    else if (action === CHECKOUT) {
+  const [emptyCart] = useMutation(EMPTY_CART_MUTATION)
+  const client = useApolloClient()
+
+  const handleFooterActions = async (e, action) => {
+    if (action === EMPTY_CART) {
+      try {
+        // eslint-disable-next-line camelcase
+        const cart_id = await getCartId(client)
+        // prettier-ignore
+        const { data: { cart: cartInfo } } = await emptyCart({ variables: { cart_id } })
+
+        client.writeData({ data: { cartInfo, cartTotalAmount: 0.0 } })
+        localStorage.removeItem(CART_ID_KEY)
+      } catch (err) {
+        // TODO: handle error
+        // eslint-disable-next-line no-console
+        console.warn(err)
+      }
+    } else if (action === CHECKOUT) {
       // TODO: handle checkout
     }
   }
